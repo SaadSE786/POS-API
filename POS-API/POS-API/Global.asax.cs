990e7dee6f04e9d6a93af37898462bddd6d2a753
Saad-Bajwa <bajwa.saad1122@gmail.com>
2025-01-30 20:04:30 +0500

    That is the implementation of the ChartOfAccount Crud apis.

--- POS-API/POS-API/Global.asax.cs
+++ POS-API/POS-API/Global.asax.cs
@@ -1,16 +1,22 @@
-﻿using System;
+﻿using POS_API.Services;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Unity;
+using Unity.Injection;
+using Unity.WebApi;
 
 namespace POS_API
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        public static SQLService _sql;
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -18,6 +24,9 @@ namespace POS_API
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            var container = new UnityContainer();
+            container.RegisterType<SQLService>(new InjectionConstructor(ConfigurationManager.ConnectionStrings["POSDatabase"].ConnectionString));
+            GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
     }
 }
